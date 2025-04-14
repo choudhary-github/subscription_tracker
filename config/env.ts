8e480834102f0520356f771161e01b8556b89991
choudhary-github <kumarankit.ak325@gmail.com>
2025-04-14 13:45:52 +0530

    refactor Arcjet middleware for improved error handling, update environment variable loading, and add start script in package.json

--- config/env.ts
+++ config/env.ts
@@ -1,3 +1,4 @@
+import "dotenv/config"
 import { config } from "dotenv";
 
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
--- middleware/arcjet.middleware.ts
+++ middleware/arcjet.middleware.ts
@@ -1,17 +1,25 @@
 import type { Request, Response, NextFunction } from "express";
 import aj from "../config/arcjet";
 
-const arcjectMiddleware = async (req: any, res: Response, next: NextFunction) => {
+const arcjectMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const decision = await aj.protect(req, { requested: 1 });
     if (decision.isDenied()) {
-      if (decision.reason.isBot()) res.status(403).send({ error: "Bot detected" });
-      if (decision.reason.isRateLimit()) res.status(429).send({ error: "Rate limit exceeded" });
-
-      res.status(403).send({ error: "Access denied" });
-      return;
+      if (decision.reason.isRateLimit()) {
+        res.status(429).send({ error: "Too Many Requests" });
+        return;
+      } else if (decision.reason.isBot()) {
+        res.status(403).send({ error: "No bots allowed" });
+        return;
+      } else {
+        res.status(403).send({ error: "Forbidden" });
+        return;
+      }
     }
-
+    // else if (decision.results.some(isSpoofedBot)) {
+    //   res.status(403).send({ error: "Forbidden" });
+    //   return;
+    // }
     next();
   } catch (error) {
     console.log("arcjectMiddleware error", error);
