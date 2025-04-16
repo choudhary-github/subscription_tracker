b043addd50becf2012df01720534d0795e17f230
choudhary-github <kumarankit.ak325@gmail.com>
2025-04-16 11:13:59 +0530

    test: adjust token refill rate in arcjet configuration for improved performance

--- config/arcjet.ts
+++ config/arcjet.ts
@@ -12,7 +12,7 @@ const aj = arcjet({
     }),
     tokenBucket({
       mode: "LIVE",
-      refillRate: 5, // Refill 5 tokens per interval
+      refillRate: 1, // Refill 5 tokens per interval
       interval: 10, // Refill every 10 seconds
       capacity: 10, // Bucket capacity of 10 tokens
     }),
